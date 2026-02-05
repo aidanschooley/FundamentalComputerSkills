706be763ed76488e177bdb0a483088dd09bdb285
aidanschooley <schooleyaidan7@gmail.com>
2026-02-04 21:52:41 -0500

    cors

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 // server.js
 import express from 'express';
 import fs from 'fs';
+import cors from 'cors';
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.get('/api/hello', (req, res) => {
   res.json({ message: 'Hello from Express backend!' });
