d8763f1d2184dba980c71efd945538b81ddb2e00
aidanschooley <schooleyaidan7@gmail.com>
2026-03-11 20:15:00 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,7 @@ import routes from './routes/index.js';
 import express from 'express';
 
 const app = express();
-const PORT = 3000;
-
+const port = process.env.PORT || 3000;
 //middleware
 app.use(express.json()); 
 
